f6cf9617bbc0de380e35707bfa26a21908a8a5e1
MFortal <mashfort@yandex.ru>
2020-04-02 20:45:28 +0300

    Добавление LevelsInitializer

---
+++ Navigator/Navigator/Models/Sourceseed/LevelsInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Navigator.Models.DataModels;
+
+namespace Navigator.Models.Sourceseed
+{
+    public class LevelsInitializer
+    {
+        public static Level[] Initialize()
+        {
+            return new Level[]
+            {
+                new Level{Id=1, Name=1},
+                new Level{Id=2, Name=2}
+            };
+        }
+    }
+}
