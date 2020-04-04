a5aefe7a23f43e93cb6bcd565415589bcae01c13
MFortal <mashfort@yandex.ru>
2020-04-04 17:42:05 +0300

    Add MapViewModel & change seed

--- Navigator/Navigator/Models/SourceSeed/TypeItemInitializer.cs
+++ Navigator/Navigator/Models/SourceSeed/TypeItemInitializer.cs
@@ -8,15 +8,8 @@ namespace Navigator.Models.SourceSeed
         {
             return new TypeItem[]
             {
-                new TypeItem{Id=1, Name="1"},
-                new TypeItem{Id=2, Name="2"},
-                new TypeItem{Id=3, Name="3"},
-                new TypeItem{Id=4, Name="4"},
-                new TypeItem{Id=5, Name="5"},
-                new TypeItem{Id=6, Name="6"},
-                new TypeItem{Id=7, Name="7"},
-                new TypeItem{Id=8, Name="8"},
-                new TypeItem{Id=9, Name="Стена"}
+                new TypeItem{Id=1, Name="Кабинет"},
+                new TypeItem{Id=2, Name="Граница"}
             };
         }
     }
---
+++ Navigator/Navigator/Models/ViewModels/MapViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Navigator.Models.ViewModels
+{
+    public class MapViewModel
+    {
+        public string LevelName { get; set; }
+        public string BuildingName { get; set; }
+        public List<ItemsLevel> ItemsLevel { get; set; }
+    }
+}
