7bea27ebf603e92d01d37ed19b43953ef29deefc
Лацын Николай <Nikolay.Latsyn@firstlinesoftware.com>
2020-05-03 20:56:34 +0300

    Добавление метода для печати в файл (для отладки)

--- Navigator/Services/DiscreteMap/DiscreteMapField.cs
+++ Navigator/Services/DiscreteMap/DiscreteMapField.cs
@@ -1,4 +1,6 @@
-﻿namespace Services.DiscreteMap
+﻿using System.IO;
+
+namespace Services.DiscreteMap
 {
     public class DiscreteMapField
     {
@@ -11,6 +13,27 @@
             _radiusVector = min;
             var sizeRadiusVector = max - min;
             Field = new Cell[sizeRadiusVector.X, sizeRadiusVector.Y, sizeRadiusVector.Level];
+        }
+
+        public void PrintToFile()
+        {
+            using (var sw = new StreamWriter(@"C:\Navigator\field.txt", append: false))
+            {
+                // levels
+                for (int l = 0; l < Field.GetLength(2); l++)
+                {
+                    sw.WriteLine($"Level {l}");
+                    for (int x = 0; x < Field.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < Field.GetLength(1); y++)
+                        {
+                            var cell = Field[x, y, l].Available ? '.' : '#';
+                            sw.Write(cell);
+                        }
+                        sw.WriteLine();
+                    }
+                }
+            }
         }
     }
 }
