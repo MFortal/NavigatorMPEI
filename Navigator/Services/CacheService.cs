c96ddb6229dbcdb64533ba022a909282a7346e2e
Лацын Николай <Nikolay.Latsyn@firstlinesoftware.com>
2020-05-03 20:57:30 +0300

    Создание кеш сервиса

---
+++ Navigator/Services/CacheService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Caching;
+using Services.Interfaces;
+using Services.Models;
+
+namespace Services
+{
+    public class CacheService : ICacheService
+    {
+        private static readonly MemoryCache Cache = MemoryCache.Default;
+
+        public T Get<T>(Guid id, Func<Guid, T> firstGetFunc) where T : BaseSmModel
+        {
+            var key = nameof(T) + id;
+            var result = (T)Cache.Get(key);
+            return result ?? firstGetFunc.Invoke(id);
+        }
+    }
+}
---
+++ Navigator/Services/Interfaces/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Services.Models;
+
+namespace Services.Interfaces
+{
+    public interface ICacheService
+    {
+        T Get<T>(Guid id, Func<Guid, T> firstGetFunc) where T : BaseSmModel;
+    }
+}
