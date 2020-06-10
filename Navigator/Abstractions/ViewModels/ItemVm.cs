a2a0720efbbafa1f5e24c09c134d0ddb95d80e5e
MFortal <mashfort@yandex.ru>
2020-06-10 14:33:08 +0300

    добавление изображений лестниц

--- Navigator/Abstractions/ViewModels/ItemVm.cs
+++ Navigator/Abstractions/ViewModels/ItemVm.cs
@@ -14,5 +14,14 @@ namespace Abstractions.ViewModels
 
         public IEnumerable<PointVm> Border { get; set; }
         public string BorderString => string.Join(" ", Border.Select(x => x.ToString()));
+
+        public int MaxX => Border.Select(x => x.X).Max();
+        public int MaxY => Border.Select(x => x.Y).Max();
+        public int MinX => Border.Select(x => x.X).Min();
+        public int MinY => Border.Select(x => x.Y).Min();
+
+        public int Width => MaxX - MinX;
+        public int Height => MaxY - MinY;
+
     }
 }
